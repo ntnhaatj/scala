b831543184fc6badaa7f6a27eb5aabfb8e6d47a3
nhat.nguyen <nhat.nguyen@grasshopperasia.com>
2022-01-13 16:05:06 +0700

    init project

---
+++ app/src/main/scala/thor/CustomerID.scala
@@ -0,0 +1,13 @@
+package thor
+
+import scala.util.Random
+
+object CustomerID {
+
+  def apply(name: String) = s"$name--${Random.nextLong}"
+
+  def unapply(customerID: String): Option[String] = {
+    val stringArray: Array[String] = customerID.split("--")
+    if (stringArray.tail.nonEmpty) Some(stringArray.head) else None
+  }
+}
